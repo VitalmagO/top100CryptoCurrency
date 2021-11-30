e46d62ffa4bd56121af35a84bcd637e596d3e02b
VitalmagO <vitalmago.job@gmail.com>
2021-11-30 13:54:40 +0600

    Add test banners Google AdMob

--- app/src/main/java/com/example/top100cryptocurrency/MainActivity.kt
+++ app/src/main/java/com/example/top100cryptocurrency/MainActivity.kt
@@ -3,14 +3,50 @@ package com.example.top100cryptocurrency
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import com.example.top100cryptocurrency.activities.AboutActivity
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.LoadAdError
+import com.google.android.gms.ads.interstitial.InterstitialAd
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback
 
 class MainActivity : AppCompatActivity() {
+    private var mInterstitialAd:InterstitialAd? = null
+    private var TAG = "MainActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var adRequest = AdRequest.Builder().build()
+        //Добавление межстраничного баннера при выходе из приложения
+        //MyBanner "ca-app-pub-9872688596127707/1448932633"
+        InterstitialAd.load(this, "ca-app-pub-3940256099942544/1033173712", adRequest, object : InterstitialAdLoadCallback() {
+            override fun onAdFailedToLoad(adError: LoadAdError) {
+                Log.d(TAG, adError?.message)
+                mInterstitialAd = null
+            }
+
+            override fun onAdLoaded(interstitialAd: InterstitialAd) {
+                Log.d(TAG, "Ad was loaded")
+                mInterstitialAd = interstitialAd
+            }
+        })
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        showAd()
+    }
+
+    private fun showAd() {
+        if (mInterstitialAd != null) {
+            mInterstitialAd?.show(this)
+        } else {
+            Log.d("TAG", "The interstitial ad wasn't ready yet.")
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
--- app/src/main/java/com/example/top100cryptocurrency/activities/AboutActivity.kt
+++ app/src/main/java/com/example/top100cryptocurrency/activities/AboutActivity.kt
@@ -4,18 +4,24 @@ import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import com.example.top100cryptocurrency.R
+import com.google.android.gms.ads.AdRequest
+import kotlinx.android.synthetic.main.activity_about.*
 
 class AboutActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about)
 
+        //возвращение на главную страницу
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
-    }
-
-    fun RateApp(view: View) {
-        startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=${applicationContext.packageName}")))
+        //добавление баннера
+        val adResult = AdRequest.Builder().build()
+        adView.loadAd(adResult)
+        //переход на страницу с рейтингом приложения
+        buttonRateApp.setOnClickListener{
+            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=${applicationContext.packageName}")))
+        }
     }
 }
